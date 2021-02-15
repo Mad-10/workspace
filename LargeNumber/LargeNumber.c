3902d1859b9d3a23538b86f26221e337e5ac774f
AlanIvan <2728946156@qq.com>
2021-02-15 14:49:13 +0800

    modified:   LargeNumber.c

--- LargeNumber/LargeNumber.c
+++ LargeNumber/LargeNumber.c
@@ -2,7 +2,7 @@
 #include <inttypes.h>
 #include <string.h>
 #include <stdlib.h>
-#define LEN 10
+#define LEN 20
 
 typedef struct LargeNUmber{            		//确认无误
 	int8_t digit[LEN + 1];
@@ -87,12 +87,12 @@ void LNScan(LN *des) {            		//确认无误
 }
 
 //统计位数
-int LNLength(LN *src) {            		//确认无误
-	int8_t *p = &(src->digit[0]);
+int LNLength(LN src) {            		//确认无误
+	int8_t *p = src.digit;
 	//找最高位
 	{
 		while(1) {
-			if(p - &(src->digit[0]) == LEN) {
+			if(p - &(src.digit[0]) == LEN) {
 				//p++;
 				break;
 			}
@@ -104,7 +104,7 @@ int LNLength(LN *src) {            		//确认无误
 		}
 	}
 	//偏移量之差
-	return &(src->digit[LEN - 1]) - p + 1;
+	return &(src.digit[LEN - 1]) - p + 1;
 }
 
 //输出
@@ -146,24 +146,26 @@ void LNPrint (LN *src) {            		//确认无误
 }
 
 //大数加法
-LN LNAdd(const LN a, const LN b) {
+LN LNAdd(LN a, LN b) {
 	LN result;
 	memset(result.digit, 0, sizeof(result.digit));
-
+	int alen = LNLength(a);
+	int blen = LNLength(b);
+	int len = (alen > blen) ? (alen) : (blen);
 	int i = 0;
 	int8_t temp = 0;
-	for (i = LEN - 1; i >= 0; i--) {
-		if (a.digit[i] == 0 && b.digit[i] == 0) {
-			break;
-		} else {
+	for (i = LEN - 1; LEN - 1 - i <= len; i--) {
 			temp = a.digit[i] + b.digit[i];
-
 			result.digit[i] += temp % 10;
-			if(temp > 9) {
-				temp /= 10;
-				result.digit[i - 1] += temp;
+			if(temp > 9 || result.digit[i] > 9) {
+				if(temp > 9) {
+					result.digit[i - 1] += temp / 10;
+				}
+				if(result.digit[i] > 9) {
+					result.digit[i - 1] += result.digit[i] / 10;
+					result.digit[i] = result.digit[i] % 10;
+				}
 			}
-		}
 	}
 	return result;
 }
@@ -173,19 +175,14 @@ LN LNSubtract (const LN a, const LN b) {
 	LN result;
 	memset(result.digit, 0, sizeof(result.digit));
 
+	int alen = LNLength(a);
+	int blen = LNLength(b);
+
+	int len = (alen > blen) ? (alen) : (blen);
+
 	int i = 0;
 	int8_t temp = 0;
-	for(i = 0; a.digit[i] == 0 && b.digit[i] == 0; i++);
-
-	//处理最高位，决定正负
-	temp = a.digit[i] - b.digit[i];
-	result.digit[i] += temp;
-	i++;
-
-	for (;i <= LEN - 1; i++) {
-		if (a.digit[i] == 0 && b.digit[i] == 0) {
-			break;
-		} else {
+	for (i = LEN - 1 - len;i <= LEN - 1; i++) {
 			temp = a.digit[i] - b.digit[i];
 
 			if (temp * result.digit[i - 1] < 0) {
@@ -198,29 +195,71 @@ LN LNSubtract (const LN a, const LN b) {
 				}
 			}
 			result.digit[i] += temp;
-		}
 	}
 	return result;
 
 }
 
 
+//大数乘法
+LN LNMultiply (LN a, LN b) {
+	LN result;
+	memset(result.digit, 0, sizeof(result.digit));
+	LN sum;
+	memset(sum.digit, 0, sizeof(sum.digit));
+	int alen = LNLength(a);//a的位数
+	int blen = LNLength(b);//b的位数
+	if (alen + blen > LEN) {
+		printf("因两数位数之和%d大于%d, 其乘积可能会超出范围进而导致数据错误。\n请调整最大位数，或输入较小的数。\n", alen + blen, LEN);
+		exit(1);
+	}
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int8_t temp = 0;
+	for (i = LEN - 1; k < blen; i--) {
+		for (j = LEN - 1; LEN - 1 - j < alen; j--) {
+			temp = b.digit[i] * a.digit[j];
+			sum.digit[j - k] += temp % 10;
+			sum.digit[j - k - 1] += temp /10;
+		}
+		k++;
+		result = LNAdd(result, sum);
+		memset(sum.digit, 0, sizeof(sum.digit));
+	}
+	return result;
+}
+
+//大数除法
+LN LNDivide (LN a, LN b) {
+
+}
 int main(void) {
 	LN a;
-	LNScan(&a);
+	memset(a.digit, 0, sizeof(a.digit));
+    LNScan(&a);
 	LN b;
-	LNScan(&b);
-
+	memset(b.digit, 0, sizeof(b.digit));
+    LNScan(&b);
+//	a.digit[LEN - 2] = 9;
+//	a.digit[LEN - 1] = 8;
+//	b.digit[LEN - 1] = 2;
+//	int i = 0;
+//	for (i = 0; i < 100; i++) {
+//		a = LNSubtract(a, b);
+//		printf("i = ");
+//		LNPrint(&a);
+//		putchar('\n');
+//	}
 	LN c;
-	c = LNSubtract(a, b);
-
+	c = LNMultiply(a, b);
 	LNPrint(&c);
-
-
 	return 0;
 }
 
 /*
 可优化：
-1：目前未考虑输入负号；
+1：目前未考虑输入负数；
+2:获取可以利用闲置的栈；
+3:利用位数计算函数；已优化
 */
